1dfd8ac045f469c600bc9e620d5abb52e4bb3694
Tony <645488505@qq.com>
2015-09-02 09:45:59 +0800

    new test

---
+++ src/DeleteNodeinaLinkedList.java
@@ -0,0 +1,9 @@
+package solution;
+
+public class DeleteNodeinaLinkedList {
+    public void deleteNode(ListNode node) {
+        node.val=node.next.val;
+    	node.next=node.next.next;
+    	
+    }
+}
